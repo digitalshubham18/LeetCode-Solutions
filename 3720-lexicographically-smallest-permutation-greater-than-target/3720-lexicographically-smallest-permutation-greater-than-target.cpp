24826b77c6e5255da4f64ebf2e45172e8b8d20eb
Shubham Kumar <shubraj2323@gmail.com>
2026-08-27 22:48:44 +0530

    Time: 3 ms (69.43%), Space: 9.9 MB (74.52%) - LeetHub

---
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    string lexGreaterPermutation(string s, string target) {
+        int cnt[26] = {};
+
+        
+        for(char ch : s) {
+            cnt[ch - 'a']++;
+        }
+
+        int i = 0;
+        while(i < target.size() && cnt[target[i] - 'a'] > 0) {
+            cnt[target[i] - 'a']--;
+            i++;
+        }
+
+        while(i >= 0) {
+
+            if(i < target.size()) {
+
+                for(int j = target[i] - 'a' + 1; j < 26; j++) {
+
+                    if(cnt[j] > 0) {
+
+                        string ans = target.substr(0, i);
+
+                        
+                        ans += char('a' + j);
+                        cnt[j]--;
+
+                        for(int k = 0; k < 26; k++) {
+                            while(cnt[k] > 0) {
+                                ans += char('a' + k);
+                                cnt[k]--;
+                            }
+                        }
+
+                        return ans;
+                    }
+                }
+            }
+
+            // Go one position back
+            if(i == 0)
+                break;
+
+            i--;
+
+            // Restore target[i]
+            cnt[target[i] - 'a']++;
+        }
+
+        return "";
+    }
+};
