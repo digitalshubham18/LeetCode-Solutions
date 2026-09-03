f154455bb15e81ba149ae8b8de8f8322705059ad
Shubham Kumar <shubraj2323@gmail.com>
2026-09-03 19:19:11 +0530

    Time: 5 ms (54.71%), Space: 165.7 MB (87.43%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -1,28 +1,47 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int Odd = INT_MAX;
-        int Even = INT_MAX;
-
-        int n = nums1.size();
-
-        for (int i = 0; i < n; i++) {
-            if (nums1[i] % 2 == 0) {
-                Even = min(Even, nums1[i]);
-            } else {
-                Odd = min(Odd, nums1[i]);
+        int even = INT_MAX;
+        int odd=INT_MAX;
+        int n=nums1.size();
+        for(int i=0;i<n;i++){
+            if(nums1[i]%2==0){
+                even=min(even,nums1[i]);
+            }
+            else{
+                odd=min(odd,nums1[i]);
             }
         }
+            if(odd==INT_MAX){
+                return true;
+            }
+            if(even==INT_MAX){
+                return true;
+        
+        }
+        return odd < even;
+        // int Odd = INT_MAX;
+        // int Even = INT_MAX;
+
+        // int n = nums1.size();
+
+        // for (int i = 0; i < n; i++) {
+        //     if (nums1[i] % 2 == 0) {
+        //         Even = min(Even, nums1[i]);
+        //     } else {
+        //         Odd = min(Odd, nums1[i]);
+        //     }
+        // }
 
         
-        if (Odd == INT_MAX) {
-            return true;
-        }
+        // if (Odd == INT_MAX) {
+        //     return true;
+        // }
 
-        if (Even == INT_MAX) {
-            return true;
-        }
+        // if (Even == INT_MAX) {
+        //     return true;
+        // }
 
-        return Odd < Even;
+        // return Odd < Even;
     }
 };
