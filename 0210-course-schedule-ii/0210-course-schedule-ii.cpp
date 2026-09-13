92dfe8110c4b2f57e039819b763098c7185cf2ec
Shubham Kumar <shubraj2323@gmail.com>
2026-09-13 19:13:06 +0530

    Time: 79 ms (5.01%), Space: 17.2 MB (99.35%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,85 @@
+class Solution {
+public:
+
+    bool isCycleDFS(int src, vector<bool>& vis,vector<bool>& recPath, vector<vector<int>>& edges) {
+
+        vis[src] = true;
+        recPath[src] = true;
+
+        for (int i = 0; i < edges.size(); i++) {
+
+            int v = edges[i][0];
+            int u = edges[i][1];
+
+            if (src == u) {
+
+                if (!vis[v]) {
+                    if (isCycleDFS(v, vis, recPath, edges))
+                        return true;
+                }
+
+                else if (recPath[v]) {
+                    return true;
+                }
+            }
+        }
+
+        recPath[src] = false;
+
+        return false;
+    }
+
+    void toporder(int src, vector<bool>& vis, stack<int>& s, vector<vector<int>>& edges) {
+
+        vis[src] = true;
+
+        for (int i = 0; i < edges.size(); i++) {
+
+            int v = edges[i][0];
+            int u = edges[i][1];
+
+            if (src == u && !vis[v]) {
+                toporder(v, vis, s, edges);
+            }
+        }
+
+        
+        s.push(src);
+    }
+
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+
+        vector<bool> vis(numCourses, false);
+        vector<bool> recPath(numCourses, false);
+
+    
+        for (int i = 0; i < numCourses; i++) {
+
+            if (!vis[i]) {
+
+                if (isCycleDFS(i, vis, recPath, prerequisites)) {
+                    return {};
+                }
+            }
+        }
+
+        vector<bool> vis2(numCourses, false);
+        stack<int> s;
+
+        for (int i = 0; i < numCourses; i++) {
+
+            if (!vis2[i]) {
+                toporder(i, vis2, s, prerequisites);
+            }
+        }
+
+        vector<int> ans;
+
+        while (!s.empty()) {
+            ans.push_back(s.top());
+            s.pop();
+        }
+
+        return ans;
+    }
+};
