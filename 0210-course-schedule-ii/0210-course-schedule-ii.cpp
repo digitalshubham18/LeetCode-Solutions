0ed513690fc9c828bd009fa3b28a9e7c5fbf385e
Shubham Kumar <shubraj2323@gmail.com>
2026-09-13 19:16:23 +0530

    Time: 5 ms (44.36%), Space: 18.5 MB (52.43%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,78 +1,60 @@
 class Solution {
 public:
 
-    bool isCycleDFS(int src, vector<bool>& vis,vector<bool>& recPath, vector<vector<int>>& edges) {
 
-        vis[src] = true;
-        recPath[src] = true;
+    bool dfs(int src, vector<vector<int>>& graph,vector<int>& state, stack<int>& s) {
 
-        for (int i = 0; i < edges.size(); i++) {
+       
+        state[src] = 1;
 
-            int v = edges[i][0];
-            int u = edges[i][1];
+        for (int i = 0; i < graph[src].size(); i++) {
 
-            if (src == u) {
+            int v = graph[src][i];
 
-                if (!vis[v]) {
-                    if (isCycleDFS(v, vis, recPath, edges))
-                        return true;
-                }
+            if (state[v] == 1) {
+                return true;
+            }
 
-                else if (recPath[v]) {
+            if (state[v] == 0) {
+                if (dfs(v, graph, state, s)) {
                     return true;
                 }
             }
         }
 
-        recPath[src] = false;
+        state[src] = 2;
+
+        s.push(src);
 
         return false;
     }
 
-    void toporder(int src, vector<bool>& vis, stack<int>& s, vector<vector<int>>& edges) {
+    vector<int> findOrder(int numCourses,vector<vector<int>>& prerequisites) {
 
-        vis[src] = true;
+        vector<vector<int>> graph(numCourses);
 
-        for (int i = 0; i < edges.size(); i++) {
+        for (int i = 0; i < prerequisites.size(); i++) {
 
-            int v = edges[i][0];
-            int u = edges[i][1];
+            int course = prerequisites[i][0];
+            int prerequisite = prerequisites[i][1];
 
-            if (src == u && !vis[v]) {
-                toporder(v, vis, s, edges);
-            }
+            graph[prerequisite].push_back(course);
         }
 
-        
-        s.push(src);
-    }
+        vector<int> state(numCourses, 0);
+        stack<int> s;
 
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-
-        vector<bool> vis(numCourses, false);
-        vector<bool> recPath(numCourses, false);
-
-    
         for (int i = 0; i < numCourses; i++) {
 
-            if (!vis[i]) {
+            if (state[i] == 0) {
 
-                if (isCycleDFS(i, vis, recPath, prerequisites)) {
+                if (dfs(i, graph, state, s)) {
                     return {};
                 }
             }
         }
 
-        vector<bool> vis2(numCourses, false);
-        stack<int> s;
-
-        for (int i = 0; i < numCourses; i++) {
-
-            if (!vis2[i]) {
-                toporder(i, vis2, s, prerequisites);
-            }
-        }
-
+        
         vector<int> ans;
 
         while (!s.empty()) {
@@ -83,3 +65,85 @@ public:
         return ans;
     }
 };
+    // bool isCycleDFS(int src, vector<bool>& vis,vector<bool>& recPath, vector<vector<int>>& edges) {
+
+    //     vis[src] = true;
+    //     recPath[src] = true;
+
+    //     for (int i = 0; i < edges.size(); i++) {
+
+    //         int v = edges[i][0];
+    //         int u = edges[i][1];
+
+    //         if (src == u) {
+
+    //             if (!vis[v]) {
+    //                 if (isCycleDFS(v, vis, recPath, edges))
+    //                     return true;
+    //             }
+
+    //             else if (recPath[v]) {
+    //                 return true;
+    //             }
+    //         }
+    //     }
+
+    //     recPath[src] = false;
+
+    //     return false;
+    // }
+
+    // void toporder(int src, vector<bool>& vis, stack<int>& s, vector<vector<int>>& edges) {
+
+    //     vis[src] = true;
+
+    //     for (int i = 0; i < edges.size(); i++) {
+
+    //         int v = edges[i][0];
+    //         int u = edges[i][1];
+
+    //         if (src == u && !vis[v]) {
+    //             toporder(v, vis, s, edges);
+    //         }
+    //     }
+
+        
+    //     s.push(src);
+    // }
+
+    // vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+
+    //     vector<bool> vis(numCourses, false);
+    //     vector<bool> recPath(numCourses, false);
+
+    
+    //     for (int i = 0; i < numCourses; i++) {
+
+    //         if (!vis[i]) {
+
+    //             if (isCycleDFS(i, vis, recPath, prerequisites)) {
+    //                 return {};
+    //             }
+    //         }
+    //     }
+
+    //     vector<bool> vis2(numCourses, false);
+    //     stack<int> s;
+
+    //     for (int i = 0; i < numCourses; i++) {
+
+    //         if (!vis2[i]) {
+    //             toporder(i, vis2, s, prerequisites);
+    //         }
+    //     }
+
+    //     vector<int> ans;
+
+    //     while (!s.empty()) {
+    //         ans.push_back(s.top());
+    //         s.pop();
+    //     }
+
+    //     return ans;
+//     }
+// };
